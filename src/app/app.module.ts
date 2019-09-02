e7cc6ab4195b05404fa082f2899c3fd20710f85b
ImsirovicAjdin <ajdika@live.com>
2019-09-02 21:51:24 +0200

    Import the usage modules and the class component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DemoTypeaheadReactiveFormComponent } from './typeahead/typeahead.component.ts';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    ReactiveFormsModule,
+    BrowserAnimationsModule, 
+    TypeaheadModule.forRoot() 
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent,
+    DemoTypeaheadReactiveFormComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
